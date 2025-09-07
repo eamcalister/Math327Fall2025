9a98f3ba4f0ede3465689b0f63db5a9aabd9ee3f
eamcalister <mcalister_e@fortlewis.edu>
2025-09-07 16:34:01 -0600

    Latest build deployed.

--- worksheets/lunr-pretext-search-index.js
+++ worksheets/lunr-pretext-search-index.js
@@ -44,6 +44,42 @@ var ptx_lunr_docs = [
   "number": "3",
   "title": "",
   "body": "  Now create an Insight to for the fish population with harvesting given in section 1.3 of the text (the differential equation is equation 1.3.6 in html version). Your Insight should have three parameters , , and . It should have two flows, one inflow for reproduction (involving ) and one outflow to account for resource limitations and harvesting (involving and ). Again, include the Insightmaker file for this exercise. To receive full credit all primitives should have units.    Sense making tip: Do a little algebra to get the differential equation to have isolated parts with obvious 's and 's and use in arrows for 's and out arrows for 's. Also, to receive full credit, your Insight must include units for all stocks, flows, and variables.     (5 points) Let , , , and . Run the simulation. Does the solution curve make sense, given the context of the model (What are and supposed to represent?)? Also, does the graph you get from Insightmaker approximately agree with the solution given by equation 1.3.7 (in html version insider reading exercise 1.3.8) of the text (using the given parameters, graphed somewhere else, like Desmos)? Include screenshots of all your images in your submission and write sentences to explain.      (5 points) By varying values of (with the other parameters as above), how large can be before the fish population goes extinct eventually? Include screenshots in your write-up to support your claim.    "
+},
+{
+  "id": "Fastpitch_Formulas",
+  "level": "1",
+  "url": "Fastpitch_Formulas.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Modeling Activity 2 (Part 1) - Analytic Approach to the Fastball Model",
+  "body": " Modeling Activity 2 (Part 1) - Analytic Approach to the Fastball Model   Pitch velocity is one of the most fascinating statistics in baseball, as documented in the 2015 documentary Fastball ( ). The interesting thing is that methods of measurement have changed over the years. Curently, the velocity is measured as is leaves the pitcher's hand. From 2008 through 2016, the velocity was measured at a distance of feet from home plate. When a radar gun was first used in a Major League game in 1974, the measurement of velocity was take feet from home plate. Since pitches slow down as they travel, we would like to develop an ODE model for pitch velocity to compare some of the fastest pitches ever thrown.   This activity is to be done with a partner and is due Friday, September 12 on Canvas. Please make sure your final write-up is in ``final draft'' form.      Define variables as follows: Assume that the force acting to slow down a baseball in flight is given by    Use dimensional analysis to explain why the exponent must equal .    Explain why it makes sense to have the minus sign in front of .       Attach units to all the terms on both sides of the given equation and observe that time units only appear in one place on the right-hand side.    From now on, we may represent the ODE given in as where .      Solve the ODE given above for and integrate to get the displacement function .        We would like to estimate the value of the parameter using the formula for from from data from a single pitch collected with a Bluetooth enabled baseball ( ). The data collected consists of the following:    Time to Plate: This is the time it takes from the pitcher's initial movement until the ball gets to home plate, which is 60.5 feet from the pitcher's mound.     Delivery Time: This is the time from the pitcher's initial movement until the ball is released.     Extension: This is how far in front of the pitcher's mound the ball is released.     Baseball Velocity Data    Initial Velocity (mph) Time to Plate (s) Delivery Time (s) Extension (ft)    85 1.2 0.74 5.1        Use the formula for you got in and the single pitch data in and try to solve for algebraically. Say where you get stuck.      Now use some graphing technology to estimate using the given data and your formula from . You must include a screenshot of your graph in your write-up .     The estimate of you just found was based on a single pitch and might be inexact. On Wednesday we will use more data and Insightmaker to refine our estimate of . Then we will have enough information to answer who threw the fastest pitch ever.   "
+},
+{
+  "id": "baseball_dimensional_model",
+  "level": "2",
+  "url": "Fastpitch_Formulas.html#baseball_dimensional_model",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Define variables as follows: Assume that the force acting to slow down a baseball in flight is given by    Use dimensional analysis to explain why the exponent must equal .    Explain why it makes sense to have the minus sign in front of .       Attach units to all the terms on both sides of the given equation and observe that time units only appear in one place on the right-hand side.   "
+},
+{
+  "id": "analytic_solution",
+  "level": "2",
+  "url": "Fastpitch_Formulas.html#analytic_solution",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Solve the ODE given above for and integrate to get the displacement function .   "
+},
+{
+  "id": "Fastpitch_Formulas-5-1",
+  "level": "2",
+  "url": "Fastpitch_Formulas.html#Fastpitch_Formulas-5-1",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  We would like to estimate the value of the parameter using the formula for from from data from a single pitch collected with a Bluetooth enabled baseball ( ). The data collected consists of the following:    Time to Plate: This is the time it takes from the pitcher's initial movement until the ball gets to home plate, which is 60.5 feet from the pitcher's mound.     Delivery Time: This is the time from the pitcher's initial movement until the ball is released.     Extension: This is how far in front of the pitcher's mound the ball is released.     Baseball Velocity Data    Initial Velocity (mph) Time to Plate (s) Delivery Time (s) Extension (ft)    85 1.2 0.74 5.1        Use the formula for you got in and the single pitch data in and try to solve for algebraically. Say where you get stuck.      Now use some graphing technology to estimate using the given data and your formula from . You must include a screenshot of your graph in your write-up .    "
 }
 ]
 
